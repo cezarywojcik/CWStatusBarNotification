9704c4a593f0dab19c9aef61c83536f097425721
Cezary Wojcik <cezarywojcik92@gmail.com>
2014-03-15 15:09:34 -0700

    2.1.1

--- CWStatusBarNotification/CWStatusBarNotification.m
+++ CWStatusBarNotification/CWStatusBarNotification.m
@@ -16,6 +16,61 @@
 #define SCROLL_SPEED 40.0f
 #define SCROLL_DELAY 1.0f
 
+# pragma mark - ScrollLabel
+
+@implementation ScrollLabel {
+    UIImageView *textImage;
+}
+
+- (id)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        textImage = [[UIImageView alloc] init];
+        [self addSubview:textImage];
+    }
+    return self;
+}
+
+- (CGFloat)fullWidth {
+    return [self.text sizeWithAttributes:@{NSFontAttributeName: self.font}].width;
+}
+
+- (CGFloat)scrollOffset {
+    if (self.numberOfLines != 1) return 0;
+    
+    CGRect insetRect = CGRectInset(self.bounds, PADDING, 0);
+    return MAX(0, [self fullWidth] - insetRect.size.width);
+}
+
+- (CGFloat)scrollTime {
+    return ([self scrollOffset] > 0) ? [self scrollOffset] / SCROLL_SPEED + SCROLL_DELAY : 0;
+}
+
+- (void)drawTextInRect:(CGRect)rect {
+    if ([self scrollOffset] > 0) {
+        rect.size.width = [self fullWidth] + PADDING * 2;
+        UIGraphicsBeginImageContextWithOptions(rect.size, NO, [UIScreen mainScreen].scale);
+        [super drawTextInRect:rect];
+        textImage.image = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        [textImage sizeToFit];
+        [UIView animateWithDuration:[self scrollTime] - SCROLL_DELAY
+                              delay:SCROLL_DELAY
+                            options:UIViewAnimationOptionBeginFromCurrentState | UIViewAnimationOptionCurveEaseInOut
+                         animations:^{
+                             textImage.transform = CGAffineTransformMakeTranslation(-[self scrollOffset], 0);
+                         } completion:^(BOOL finished) {
+                         }];
+    } else {
+        textImage.image = nil;
+        [super drawTextInRect:CGRectInset(rect, PADDING, 0)];
+    }
+}
+
+@end
+
+# pragma mark - CWStatusBarNotification
+
 @implementation CWStatusBarNotification
 
 @synthesize notificationLabel, notificationLabelBackgroundColor, notificationLabelTextColor, notificationWindow;
@@ -281,59 +336,5 @@
         });
     }];
 }
-
-@end
-
-# pragma mark - ScrollLabel
-
-@implementation ScrollLabel {
-    UIImageView *textImage;
-}
-
-- (id)initWithFrame:(CGRect)frame {
-    self = [super initWithFrame:frame];
-    if (self) {
-        textImage = [[UIImageView alloc] init];
-        [self addSubview:textImage];
-    }
-    return self;
-}
-
-- (CGFloat)fullWidth {
-    return [self.text sizeWithAttributes:@{NSFontAttributeName: self.font}].width;
-}
-
-- (CGFloat)scrollOffset {
-    if (self.numberOfLines != 1) return 0;
-
-    CGRect insetRect = CGRectInset(self.bounds, PADDING, 0);
-    return MAX(0, [self fullWidth] - insetRect.size.width);
-}
-
-- (CGFloat)scrollTime {
-    return ([self scrollOffset] > 0) ? [self scrollOffset] / SCROLL_SPEED + SCROLL_DELAY : 0;
-}
-
-- (void)drawTextInRect:(CGRect)rect {
-    if ([self scrollOffset] > 0) {
-        rect.size.width = [self fullWidth] + PADDING * 2;
-        UIGraphicsBeginImageContextWithOptions(rect.size, NO, [UIScreen mainScreen].scale);
-        [super drawTextInRect:rect];
-        textImage.image = UIGraphicsGetImageFromCurrentImageContext();
-        UIGraphicsEndImageContext();
-        [textImage sizeToFit];
-        [UIView animateWithDuration:[self scrollTime] - SCROLL_DELAY
-                              delay:SCROLL_DELAY
-                            options:UIViewAnimationOptionBeginFromCurrentState | UIViewAnimationOptionCurveEaseInOut
-                         animations:^{
-                             textImage.transform = CGAffineTransformMakeTranslation(-[self scrollOffset], 0);
-                         } completion:^(BOOL finished) {
-                         }];
-    } else {
-        textImage.image = nil;
-        [super drawTextInRect:CGRectInset(rect, PADDING, 0)];
-    }
-}
-
 
 @end
--- README.md
+++ README.md
@@ -14,7 +14,7 @@ Works for iPhone and iPad.
 
 ### CocoaPods
 
-`pod 'CWStatusBarNotification', '~> 2.1.0'`
+`pod 'CWStatusBarNotification', '~> 2.1.1'`
 
 ### Manual
 
@@ -115,5 +115,3 @@ The notifications will work in both screen orientations, however, screen rotatio
     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
     THE SOFTWARE.
-
-
